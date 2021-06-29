651fc95079b9621ae46aed607e6baf07c2df11ef
Henrik Cooke <henrik@cooke.se>
2021-06-29 09:18:51 +0200

    Fixed incorrect context. Remove unused serializer.

--- dotnet-server/CookeRpc.AspNetCore/BuilderExtensions.cs
+++ dotnet-server/CookeRpc.AspNetCore/BuilderExtensions.cs
@@ -20,7 +20,7 @@ namespace CookeRpc.AspNetCore
 
         public static IApplicationBuilder UseRpc(this IApplicationBuilder app, string path = "/rpc")
         {
-            var model = new RpcModel(new() {ContextType = typeof(HttpContent)});
+            var model = new RpcModel(new() {ContextType = typeof(HttpRpcContext)});
             model.AddRpcServicesByAttribute();
             return UseRpc(app, model, path);
         }
